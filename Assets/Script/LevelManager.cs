e4aa3aa506c115a216c5c7b02b02c584623beaad
Tony12X <dsouzaanthony781@gmail.com>
2025-06-01 14:15:07 +0530

    Release v1.0

--- Assets/Script/LevelManager.cs
+++ Assets/Script/LevelManager.cs
@@ -1,24 +1,58 @@
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
+using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
+using UnityEngine.InputSystem;
+using System.Collections;
 
 public class LevelManager : MonoBehaviour
 {
     public Transform player;
     public DynamicMoveProvider _dynamicMoveProvider;
 
+    public List<Animator> scrollAnimators = new List<Animator>();
     [Header("Transform")]
     public Transform startPosition;
     public Transform ScrollPosition_1;
+    public List<Transform> ScrollPositions = new List<Transform>();
 
     [Header("Audio")]
     public AudioSource _audioSource;
+    public AudioClip _bgAudioClip;
+    public List<AudioClip> _audioClips = new List<AudioClip>();
 
     [Header("UI")]
     public GameObject _StartMenu;
     public GameObject _GameUI;
+    public GameObject TeleportPanel;
     public Slider _audioSlider;
+
+    private bool EnableScrollSequence;
+    public InputActionReference A_Buton;
+    public InputActionReference X_Buton;
+    enum ScrollCount { scroll1,scroll2,scroll3,scroll4,scroll5,scroll6,scroll7,scroll8,scroll9,sacroll10};
+    private ScrollCount myScrollCount;
+
+    private float _Time;
+    private float _nextClipPlayTime;
+    private void Update()
+    {
+        _Time += Time.deltaTime;
+
+        if (A_Buton.action.WasPressedThisFrame() && _GameUI.activeSelf)
+        {
+            TeleportPanel.SetActive(!TeleportPanel.activeSelf);
+        }
+
+        //if (X_Buton.action.WasPressedThisFrame())
+        //{
+        //    _nextClipPlayTime = _Time + _audioClips[0].length + .5f;
+        //    EnableScrollSequence = true;
+        //    _audioSource.Stop();
+        //}
+
+        //EnableScrollSquence();
+    }
     public void OnClickStart()
     {
         player.position = ScrollPosition_1.position;
@@ -48,5 +82,187 @@ public class LevelManager : MonoBehaviour
     public void QuitApp()
     {
         Application.Quit();
+    }
+    
+    public void TeleportTo(int i)
+    {
+        player.position = ScrollPositions[i].position;
+        player.rotation = ScrollPositions[i].rotation;
+        TeleportPanel.SetActive(false);
+    }
+
+    private void EnableScrollSquence()
+    {
+
+        if (!EnableScrollSequence) return;
+
+        _audioSource.loop = false;
+
+        switch (myScrollCount)
+        {
+            case ScrollCount.scroll1:
+                scrollAnimators[0].SetBool("View", true);
+                if(!_audioSource.isPlaying)StartCoroutine(playScrollAudio(_audioClips[0], 0.5f));
+
+                SetPlayerPosition(ScrollPositions[0]);
+
+                if (_Time > _nextClipPlayTime)
+                {
+                    _audioSource.Stop();
+                    SetPlayerPosition(ScrollPositions[1]);
+
+                    scrollAnimators[0].SetBool("View", false);
+                    _nextClipPlayTime = _Time + _audioClips[1].length + .5f;
+                    myScrollCount = ScrollCount.scroll2;
+
+                }
+                break;
+            case ScrollCount.scroll2:
+                scrollAnimators[1].SetBool("View", true);
+                if (!_audioSource.isPlaying) StartCoroutine(playScrollAudio(_audioClips[1], 0.5f));
+
+                if (_Time > _nextClipPlayTime)
+                {
+                    _audioSource.Stop();
+                    SetPlayerPosition(ScrollPositions[2]);
+
+                    scrollAnimators[1].SetBool("View", false);
+                    _nextClipPlayTime = _Time + _audioClips[2].length + .5f;
+                    myScrollCount = ScrollCount.scroll3;
+                }
+                break;
+            case ScrollCount.scroll3:
+                scrollAnimators[2].SetBool("View", true);
+                StartCoroutine(playScrollAudio(_audioClips[2], 0.5f));
+
+                if (_Time > _nextClipPlayTime)
+                {
+                    _audioSource.Stop();
+                    SetPlayerPosition(ScrollPositions[3]);
+
+                    scrollAnimators[2].SetBool("View", false);
+                    _nextClipPlayTime = _Time + _audioClips[3].length + .5f;
+                    myScrollCount = ScrollCount.scroll4;
+                }
+                break;
+            case ScrollCount.scroll4:
+                scrollAnimators[3].SetBool("View", true);
+                StartCoroutine(playScrollAudio(_audioClips[3], 0.5f));
+
+                if (_Time > _nextClipPlayTime)
+                {
+                    _audioSource.Stop();
+                    SetPlayerPosition(ScrollPositions[4]);
+
+                    scrollAnimators[3].SetBool("View", false);
+                    _nextClipPlayTime = _Time + _audioClips[4].length + .5f;
+                    myScrollCount = ScrollCount.scroll5;
+                }
+                break;
+            case ScrollCount.scroll5:
+                scrollAnimators[4].SetBool("View", true);
+                StartCoroutine(playScrollAudio(_audioClips[4], 0.5f));
+
+
+                if (_Time > _nextClipPlayTime)
+                {
+                    _audioSource.Stop();
+                    SetPlayerPosition(ScrollPositions[5]);
+
+                    scrollAnimators[4].SetBool("View", false);
+                    _nextClipPlayTime = _Time + _audioClips[5].length + .5f;
+                    myScrollCount = ScrollCount.scroll6;
+                }
+                break;
+            case ScrollCount.scroll6:
+                scrollAnimators[5].SetBool("View", true);
+                StartCoroutine(playScrollAudio(_audioClips[5], 0.5f));
+
+                if (_Time > _nextClipPlayTime)
+                {
+                    _audioSource.Stop();
+                    SetPlayerPosition(ScrollPositions[6]);
+
+                    scrollAnimators[5].SetBool("View", false);
+                    _nextClipPlayTime = _Time + _audioClips[6].length + .5f;
+                    myScrollCount = ScrollCount.scroll7;
+                }
+                break;
+            case ScrollCount.scroll7:
+                scrollAnimators[6].SetBool("View", true);
+                StartCoroutine(playScrollAudio(_audioClips[6], 0.5f));
+
+                if (_Time > _nextClipPlayTime)
+                {
+                    _audioSource.Stop();
+                    SetPlayerPosition(ScrollPositions[7]);
+
+                    scrollAnimators[6].SetBool("View", false);
+                    _nextClipPlayTime = _Time + _audioClips[7].length + .5f;
+                    myScrollCount = ScrollCount.scroll8;
+                }
+                break;
+            case ScrollCount.scroll8:
+                scrollAnimators[7].SetBool("View", true);
+                StartCoroutine(playScrollAudio(_audioClips[7], 0.5f));
+
+                if (_Time > _nextClipPlayTime)
+                {
+                    _audioSource.Stop();
+                    SetPlayerPosition(ScrollPositions[8]);
+
+                    scrollAnimators[7].SetBool("View", false);
+                    _nextClipPlayTime = _Time + _audioClips[8].length + .5f;
+                    myScrollCount = ScrollCount.scroll9;
+                }
+                break;
+            case ScrollCount.scroll9:
+                scrollAnimators[8].SetBool("View", true);
+                StartCoroutine(playScrollAudio(_audioClips[8], 0.5f));
+
+                if (_Time > _nextClipPlayTime)
+                {
+                    _audioSource.Stop();
+                    SetPlayerPosition(ScrollPositions[9]);
+
+                    scrollAnimators[8].SetBool("View", false);
+                    _nextClipPlayTime = _Time + _audioClips[9].length + .5f;
+                    myScrollCount = ScrollCount.sacroll10;
+                }
+                break;
+            case ScrollCount.sacroll10:
+                scrollAnimators[9].SetBool("View", true);
+                StartCoroutine(playScrollAudio(_audioClips[9], 0.5f));
+
+
+                if (_Time > _nextClipPlayTime)
+                {
+                    _dynamicMoveProvider.enabled = true;
+                    scrollAnimators[9].SetBool("View", false);
+                    EnableScrollSequence = false;
+
+                }
+                break;
+        }
+    }
+    IEnumerator playScrollAudio(AudioClip clip, float time)
+    {
+        _audioSource.clip = null;
+        yield return new WaitForSeconds(time);
+        if (!_audioSource.isPlaying || _audioSource.clip != clip)
+        {
+            _audioSource.clip = clip;
+            _audioSource.Play();
+            Debug.Log(clip.name);
+        }
+
+    }
+
+    private void SetPlayerPosition(Transform targetPosition)
+    {
+        player.position = targetPosition.position;
+        player.rotation = targetPosition.rotation;
+
+        _dynamicMoveProvider.enabled = false;
     }
 }
--- Assets/Script/ScrollInteraction.cs
+++ Assets/Script/ScrollInteraction.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class ScrollInteraction : MonoBehaviour
@@ -8,6 +9,10 @@ public class ScrollInteraction : MonoBehaviour
 
     public LayerMask playerMask;
 
+    public AudioSource _audioSource;
+    public AudioSource _scrollAudioSource;
+    public AudioClip _bgAudioClip;
+    public AudioClip _scrollAudioClip;
 
     public Animator _anim;
     private void Update()
@@ -21,5 +26,24 @@ public class ScrollInteraction : MonoBehaviour
         Close_Button.SetActive(value);
 
         _anim.SetBool("View", value);
+
+        if (value)
+        {
+            StartCoroutine(playAudio(_scrollAudioSource, _scrollAudioClip, .5f));
+        }
+        else
+        {
+            _scrollAudioSource.Stop();
+            _audioSource.volume = 1;
+        }
+    }
+    IEnumerator playAudio(AudioSource source, AudioClip clip,float time)
+    {
+        yield return new WaitForSeconds(time);
+        if (source != null && clip != null)
+        {
+            _audioSource.volume = 0.2f;
+            source.PlayOneShot(clip);
+        }
     }
 }
